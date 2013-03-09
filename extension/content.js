4c80cfcb2541eeae3983d3171d1c52e9bd256050
JiriKrizek <jkrizek@gmail.com>
2013-03-09 17:25:14 +0100

    Fixed support for roumenovomaso

--- extension/content.js
+++ extension/content.js
@@ -2,7 +2,10 @@ var KEY_LEFT = 37;
 var KEY_RIGHT = 39;
 
 function getButton(text) {
-  var buttons = document.getElementsByClassName("roumingButton");
+  var buttons = document.getElementsByClassName("masoButton");
+  if (buttons.length==0) {
+    var buttons = document.getElementsByClassName("roumingButton");
+  }
 
   for (var i = 0; i < buttons.length; i++) {
     var hrf = buttons[i].children[0];
